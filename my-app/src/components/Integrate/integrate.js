7346e3d50f41959c75cf9c36b5783fcd2f71225d
iamfoat <fasaitamrit12@gmail.com>
2023-11-07 10:58:46 +0700

    .

---
+++ my-app/src/components/Integrate/integrate.js
@@ -0,0 +1,18 @@
+import React,{ Component } from "react";
+import { evaluate } from "mathjs";
+import { Button,FormControl } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+
+class integrate extends Component{
+    
+    calintegrate(){
+
+    }
+    
+    render(){
+        return(
+
+        )
+    }
+    
+}
--- my-app/src/components/Root/Bisection.js
+++ my-app/src/components/Root/Bisection.js
@@ -15,36 +15,46 @@ const Div = {
   margin: "0 auto",
 };
 
-class Bisection extends Component{
-   
-  
-  CalBisection(){
-    var y = document.getElementById("fx").value;
+class Bisection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      equation: "",
+      result: null,
+      check: "",
+    };
+  }
+
+  CalBisection = () => {
+    var equation = document.getElementById("fx").value;
     var xl = parseFloat(document.getElementById("xl").value);
     var xr = parseFloat(document.getElementById("xr").value);
     var scope;
     var yr, xm, ym;
 
-    if(xl!==0 || xr!==0){
-    xm = (xl + xr) / 2;
-    scope = { x: xr };
-    yr = evaluate(y, scope);
-    scope = { x: xm };
-    ym = evaluate(y, scope);
-
-    while (Math.abs(ym) >= 0.000001) {
-      if (ym * yr > 0) {
-        xr = xm;
-      } else {
-        xl = xm;
-      }
+    if (xl !== 0 || xr !== 0) {
       xm = (xl + xr) / 2;
+      scope = { x: xr };
+      yr = evaluate(equation, scope);
       scope = { x: xm };
-      ym = evaluate(y, scope);
+      ym = evaluate(equation, scope);
+
+      while (Math.abs(ym) >= 0.000001) {
+        if (ym * yr > 0) {
+          xr = xm;
+        } else {
+          xl = xm;
+        }
+        xm = (xl + xr) / 2;
+        scope = { x: xm };
+        ym = evaluate(equation, scope);
+      }
+      var y = equation.replace(/x/g, xm); 
+      this.setState({ equation: y, result: xm, check: `${y} = ${xm}` });
+
+      document.getElementById("ans").innerHTML = xm;
     }
-    document.getElementById("ans").innerHTML = xm;
-  }
-}
+  };
 
   render() {
     return (
@@ -52,19 +62,22 @@ class Bisection extends Component{
         <div style={mainDiv}>
           <div style={Div}>
             <div>
-            <h1> Root Of Equation : Bisection </h1>
+              <h1> Root Of Equation : Bisection </h1>
               <Form.Control id="fx" type="text" placeholder="input f(x)" style={{ width: "20%", margin: "0 auto" }}></Form.Control>
               <br />
               <Form.Control id="xl" type="number" placeholder="input xl" style={{ width: "20%", margin: "0 auto" }}></Form.Control>
               <br />
               <Form.Control id="xr" type="number" placeholder="input xr" style={{ width: "20%", margin: "0 auto" }}></Form.Control>
-              
+
               <br />
               <br />
               <Button class="btn" onClick={this.CalBisection} style={{ width: "10", margin: "0 auto" }}> Calculate </Button>
               <br />
               <br />
               <h id="ans"></h>
+              <br />
+              <br />
+              <div>{this.state.check}</div>
             </div>
           </div>
         </div>
@@ -73,3 +86,4 @@ class Bisection extends Component{
   }
 }
 export default Bisection;
+
